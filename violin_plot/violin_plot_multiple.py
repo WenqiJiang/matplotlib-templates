f74ea1a2f697fe3cdf78f3a643d6ea871643c3f1
WenqiJiang <wenqi.j@columbia.edu>
2021-05-07 09:40:36 +0200

    add seaborn bar plot

--- violin_plot/violin_plot_multiple.py
+++ violin_plot/violin_plot_multiple.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+
 from matplotlib.ticker import FuncFormatter
 import pandas as pd
 import seaborn as sns 
@@ -8,14 +9,18 @@ import seaborn as sns
 # Seaborn version >= 0.11.0
 
 sns.set_theme(style="whitegrid")
-
+# Set the palette to the "pastel" default palette:
+# sns.set_palette("pastel")
 
 ### Note: For violin graph, a single violin's data must be in the same column
 ###   e.g., given 3 violin plots, each with 100 points, the shape of the array
 ###   should be (100, 3), where the first column is for the first violin and so forth
 # fake up some data
+
+num_category = 10
+
 data = []
-for i in range(3):
+for i in range(num_category):
     spread = np.random.rand(50) * 100
     center = np.ones(25) * 50
     flier_high = np.random.rand(10) * 100 + 100
@@ -23,19 +28,20 @@ for i in range(3):
     partial_data = np.concatenate((spread, center, flier_high, flier_low))
     data.append(partial_data)
 
-reshaped_data = np.zeros((data[0].shape[0], 3))
+reshaped_data = np.zeros((data[0].shape[0], num_category))
 
-for i in range(3):
+for i in range(num_category):
     reshaped_data[:, i] = data[i]
 
 data = reshaped_data
 print(data.shape)
 
+plt.figure(figsize=(12, 3))
 # API: https://seaborn.pydata.org/generated/seaborn.violinplot.html
 # inner{“box”, “quartile”, “point”, “stick”, None}, optional
 ax = sns.violinplot(data=data, scale='area', inner='box')
 
-x_tick_labels = ["A", "B", "C"]
+x_tick_labels = ["Cat {}".format(i + 1) for i in range(num_category)]
 ax.set_xticklabels(x_tick_labels)
 # ax.set_yticklabels(y_tick_labels)
 # plt.yticks(rotation=0)
