dd6181924347af03181eb16ab3a4010f2a3f5b82
WenqiJiang <wenqi.j@columbia.edu>
2023-01-29 12:00:09 +0800

    update

--- plot_with_style/plot_seaborn_dont_use.py
+++ plot_with_style/plot_seaborn_dont_use.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.ticker import FuncFormatter
 
-plt.style.use('seaborn-colorblind')
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
+# plt.style.use('ggplot')
 
 if __name__ == "__main__":
 
@@ -13,15 +16,19 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots(1, 1, figsize=(8, 3))
 
+    plot0 = sns.lineplot(x=data_x, y=data_y_plot0, ax=ax)
+    plot1 = sns.lineplot(x=data_x, y=data_y_plot1, ax=ax)
+
     label_font = 16
     markersize = 8
     tick_font = 14
     color_plot0 = "#008A00"
     color_plot1 = "#1BA1E2"
 
-    plot0 = ax.plot(data_x, data_y_plot0, marker='o', markersize=markersize) #color=color_plot0, 
-    plot1 = ax.plot(data_x, data_y_plot1, marker='^', markersize=markersize) # color=color_plot1,
-    ax.legend([plot0[0], plot1[0]], ["plot0_legend", "plot1_legend"], loc='upper right', fontsize=label_font)
+    # plot0 = ax.plot(data_x, data_y_plot0)#, color=color_plot0, marker='o', markersize=markersize)
+    # plot1 = ax.plot(data_x, data_y_plot1)#, color=color_plot1, marker='^', markersize=markersize)
+    ax.legend([plot0, plot1], ["plot0_legend", "plot1_legend"], loc='upper right', fontsize=label_font)
+    # ax.legend([plot0[0], plot1[0]], ["plot0_legend", "plot1_legend"], loc='upper right', fontsize=label_font)
     ax.tick_params(top=False, bottom=True, left=True, right=False, labelleft=True, labelsize=tick_font)
     ax.get_xaxis().set_visible(True)
     ax.set_xlabel('xlabel', fontsize=label_font)
@@ -32,5 +39,5 @@ if __name__ == "__main__":
 
     plt.rcParams.update({'figure.autolayout': True})
 
-    plt.savefig('../images/plot_with_style.png', transparent=False, dpi=200, bbox_inches="tight")
+    plt.savefig('../images/plot_with_style_seaborn.png', transparent=False, dpi=200, bbox_inches="tight")
     plt.show()
